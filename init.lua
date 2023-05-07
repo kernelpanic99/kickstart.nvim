233d654ac411f3b29090fc4370d6fdbd872692ec
Arthur Nikolaienko <wrestlingtrailsheep@proton.me>
2023-05-07 22:00:18 +0300

    Disable fugitive, add lazygit

--- init.lua
+++ init.lua
@@ -70,8 +70,7 @@ require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
   -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
+  'kdheepak/lazygit.nvim',
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
